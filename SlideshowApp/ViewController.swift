df41a63e9e1f14189f4cb0e60ef6881dce0423a3
勝木えり <katsukieri@katsukierinomacbook-air.local>
2018-12-26 11:25:38 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 勝木えり on 2018/12/26.
+//  Copyright © 2018 eri.katsuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
